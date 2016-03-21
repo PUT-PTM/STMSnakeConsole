d509edf0580fb14d611bac70ac25c67e11de6981
Michal Suchorzynski <ms09081995@o2.pl>
2016-03-21 18:06:36 +0100

    Test na diodach

---
+++ main.c
@@ -0,0 +1,84 @@
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4_discovery_lis302dl.h"
+
+int a=0;
+int8_t acc_x,acc_y,acc_z;
+int main(void)
+{
+	SystemInit();
+
+	/* GPIOD Periph clock enable */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+
+
+	GPIO_InitTypeDef  GPIO_InitStructure;
+	/* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+	//Akcelerometr
+	LIS302DL_InitTypeDef LIS302DL_InitStruct;
+	LIS302DL_InitStruct.Power_Mode=LIS302DL_LOWPOWERMODE_ACTIVE;
+	LIS302DL_InitStruct.Output_DataRate=LIS302DL_DATARATE_100;
+	LIS302DL_InitStruct.Axes_Enable=LIS302DL_XYZ_ENABLE;
+	LIS302DL_InitStruct.Full_Scale=LIS302DL_FULLSCALE_2_3;
+	LIS302DL_InitStruct.Self_Test = LIS302DL_SELFTEST_NORMAL;
+	LIS302DL_Init(&LIS302DL_InitStruct);
+
+	unsigned int i;
+	int b=0;
+	for(;;)
+	{
+		for(int i = 0; i < 10000; i++) {}
+
+		LIS302DL_Read(&acc_x,LIS302DL_OUT_X_ADDR,1);
+		LIS302DL_Read(&acc_y,LIS302DL_OUT_Y_ADDR,1);
+		LIS302DL_Read(&acc_z,LIS302DL_OUT_Z_ADDR,1);
+		 float os_x = (acc_x*2.3*9.81)/127;
+		 float os_y = (acc_y*2.3*9.81)/127;
+		 float os_z = (acc_z*2.3*9.81)/127;
+
+		 if(os_x>1) GPIO_SetBits(GPIOD,GPIO_Pin_14);
+		 else GPIO_ResetBits(GPIOD,GPIO_Pin_14);
+		 if(os_x<-1) GPIO_SetBits(GPIOD,GPIO_Pin_12);
+		 else GPIO_ResetBits(GPIOD,GPIO_Pin_12);
+		 if(os_y>1) GPIO_SetBits(GPIOD,GPIO_Pin_13);
+		 else GPIO_ResetBits(GPIOD,GPIO_Pin_13);
+		 if(os_y<-1) GPIO_SetBits(GPIOD,GPIO_Pin_15);
+		 else GPIO_ResetBits(GPIOD,GPIO_Pin_15);
+
+		a=GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0);
+		for(int i=0;i<3000000;i++){}
+
+
+		/*if(a==1)
+		{
+			if(b==0)
+			{
+				b=1;
+			}
+			else
+			{
+				b=0;
+			}
+			a=0;
+		}
+		if(b==0)
+		{
+			GPIO_SetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+		}
+		else
+		{
+			GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+		}*/
+	}
+}
